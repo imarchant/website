48e8c52ddca2d0938c6a9e177c944ca20a9e5ad1
imfleethealth <isaac.marchant@fleethealth.com>
2026-01-20 16:04:17 +0000

    Update base path to /website/

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/iam/', // GitHub repo name
+  base: '/website/', // GitHub repo name
 })
